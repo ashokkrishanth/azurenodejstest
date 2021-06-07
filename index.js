1b67e7e3b99b77574e5ac24c06154ac349a6d3c2
Ashok <asokperson@gmail.com>
2021-06-07 19:10:47 +0530

    second

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const express = require("express");
+const app = express();
 
+var port = process.env.PORT || 3000;
 app.get('/test', (req,res) => res.send('Hello testing123 Ashok Pandian'));
+app.listen(port, () => console.log('Server is running on port'+port));
