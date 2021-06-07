6cfd3917551a0be8e5312e0a3356c0bfb5fe4b3d
Ashok <asokperson@gmail.com>
2021-06-07 19:04:39 +0530

    second commit

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-const http = require('http');
-const port = process.env.PORT || 3000;
+const express = require("express");
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  const msg = 'Hello Node!\n'
-  res.end(msg);
-});
-
-server.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}/`);
-});
+app.get('/test', (req,res) => res.send('Hello testing123 Ashok Pandian'));
